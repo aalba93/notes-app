ad24d42287b9921a771d5e298f3b7343913096a9
Andres Alba <andres_aalba@hotmail.com>
2019-03-07 21:28:58 -0500

    Add validator module (Lecture 11)

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const validator = require('validator');
 const getNotes = require('./notes');
 
 const msg = getNotes();
 
 console.log(msg);
+
+// console.log(validator.isEmail('andresexample.com'));
+
+console.log(validator.isURL('example.com'));
